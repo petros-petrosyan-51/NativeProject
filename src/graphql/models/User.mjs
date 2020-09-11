a16a82ab10373cc715b5ed538de1bcb6aaa78348
petros-petrosyan-51 <petros-petrosyan-51@mail.ru>
2020-09-11 16:14:19 +0400

    initial commit

--- src/graphql/models/User.mjs
+++ src/graphql/models/User.mjs
@@ -4,6 +4,8 @@ const alertSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String,
-    valid: String
+    valid: String,
+    friends: [String],
+    favorites: [String]
 });
 export default mongoose.model('user', alertSchema)
--- src/graphql/types/Users.mjs
+++ src/graphql/types/Users.mjs
@@ -1,5 +1,6 @@
 import apolloServerModule from 'apollo-server-express'
 import user from '../models/User.mjs'
+
 import md5 from "md5";
 const gql = apolloServerModule.gql
 
@@ -10,20 +11,30 @@ export const type = gql`
        email: String!
        valid: String!
     }
+    
     extend type Query {
        getUser(id: String):userData!
+       searchUser(id: String,str: String):[userData]!
     }
     `;
 export const resolvers = {
     Query:{
-        getUser: async (_,{id})=>{
-            console.log(id)
-            return {
-                id: id,
-                username: 'Petros',
-                email: 'petros-petrosyan-51@mail.ru',
-                valid: ''
-            };
+        getUser: (_,{id})=>{
+            return user.findOne({ _id: id});
+        },
+        searchUser: async (_,{id,str})=>{
+            const {friends} = await user.findOne({_id: id},'friends');
+            friends.push(id);
+            let result=await user.find({$or: [{username:  {$regex: new RegExp( str, "i")}},{email:  {$regex: new RegExp( str, "i")}}]},'id username email valid')
+            result.length=7;
+            friends.map(function (item){
+               result.map(function (elem,index){
+                   if (elem.id.toString()===item.toString()){
+                       result.splice(index,1);
+                   }
+                })
+            })
+            return result
         }
     },
 
